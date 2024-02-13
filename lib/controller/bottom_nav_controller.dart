964f2fc2eb14b635540efa5ced892138e51741a2
SaloniPardeshi1412 <saloni.p@binyuga.com>
2024-02-13 16:52:02 +0530

    asset added, run issue solved.

--- lib/controller/bottom_nav_controller.dart
+++ lib/controller/bottom_nav_controller.dart
@@ -1,11 +1,15 @@
 import 'package:get/get.dart';
 
+import '../pages/article_page.dart';
+import '../pages/home_page.dart';
+import '../pages/profile_page.dart';
+
 class BottomNavController extends GetxController {
   RxInt index = 0.obs;
 
   var pages = [
-    //  HomePage(),
-    // const ArticalPage(),
-    // const ProfilePage(),
+     HomePage(),
+    const ArticalPage(),
+    const ProfilePage(),
   ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:getx_demo_app/pages/home_page.dart';
 
 import 'config/theme.dart';
+import 'home_page_controller.dart';
 
 void main() {
   runApp(const MyApp());
